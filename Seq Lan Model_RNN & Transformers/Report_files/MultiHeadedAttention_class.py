573b474b2513ac55e95e8ce43fd5a7e1d7cd7203
Masoud Karami <35896090+Masoud-Karami@users.noreply.github.com>
2021-05-07 01:06:58 -0400

    Add files via upload

---
+++ Seq Lan Model_RNN & Transformers/Report_files/MultiHeadedAttention_class.py
@@ -0,0 +1,29 @@
+class MultiHeadedAttention(nn.Module):
+    def __init__(self, n_heads, n_units, dropout=0.1):
+        """
+        n_heads: the number of attention heads
+        n_units: the number of output units
+        dropout: probability of DROPPING units
+        """
+        super(MultiHeadedAttention, self).__init__()
+        # This sets the size of the keys, values, and queries (self.d_k) to all 
+        # be equal to the number of output units divided by the number of heads.
+        self.d_k = n_units // n_heads
+        # This requires the number of n_heads to evenly divide n_units.
+        assert n_units % n_heads == 0
+        self.n_units = n_units 
+
+        # TODO: create/initialize any necessary parameters or layers
+        # Note: the only Pytorch modules you are allowed to use are nn.Linear 
+        # and nn.Dropout
+        
+    def forward(self, query, key, value, mask=None):
+        # TODO: implement the masked multi-head attention.
+        # query, key, and value all have size: (batch_size, seq_len, self.n_units)
+        # mask has size: (batch_size, seq_len, seq_len)
+        # As described in the .tex, apply input masking to the softmax 
+        # generating the "attention values" (i.e. A_i in the .tex)
+        # Also apply dropout to the attention values.
+
+        return # size: (batch_size, seq_len, self.n_units)
+    
